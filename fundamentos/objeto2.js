8f1c74a47b64d6f4e5c80b8c2cb8f3cbd838c0fb
Pâmella Jucá <pamellajucas@gmail.com>
2024-05-23 23:34:13 -0300

    adicionando objetos2.js

---
+++ fundamentos/objeto2.js
@@ -0,0 +1,13 @@
+
+
+console.log(typeof Object) //retorna uma função
+console.log(typeof new Object) // instanciando uma função
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto{} //ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
+
