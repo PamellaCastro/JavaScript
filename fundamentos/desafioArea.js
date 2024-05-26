b061c62988783dea8b2eb239868be4f421cd0d67
Pâmella Jucá <pamellajucas@gmail.com>
2024-05-25 22:03:22 -0300

    inserçao dos arquivos atribuicao.js, desafioArea.js, desafioTroca.js, index.js, tipagem.js

---
+++ fundamentos/desafioArea.js
@@ -0,0 +1,8 @@
+//Pi * area * area  = formula da circunferencia
+// calcular o raio da circunferencia.
+
+const PI = 3.141592;
+const raio = 10;
+let araeCirc = PI * raio * raio;
+
+console.log("O valor da area é : " + araeCirc + "m2.");
---
+++ fundamentos/tipagem.js
@@ -0,0 +1,16 @@
+// typeof: diz o tipo da variavel, e no caso do tipo number não a diferencição entre número inteiro e número decimal
+
+let idade = 31;
+console.log(typeof idade);
+
+let salario = 2500.30;
+console.log(typeof salario);
+
+let estaChovendo = true // false
+console.log(typeof estaChovendo);
+
+let a = 3;
+
+a =10;
+
+console.log(a);
