17933e5887dc1b55815c6f35071afb5288d6a538
Pamella <pamellajucas@gmail.com>
2024-05-26 21:58:15 -0300

    atualização atribuicao.js

--- fundamentos/atribuicao.js
+++ fundamentos/atribuicao.js
@@ -1,5 +1,17 @@
-const a = 7
+const a = 7 //o simbolo de (=) é chamado atribuição, a recebe 7
 let b = 3
 
-b += a // b = b + a
+b += a // b = b + a //atribuição com soma
+console.log(b)
+
+b -= 4 //b = b -4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2  // b = b % 2 resto da divisão
 console.log(b)
